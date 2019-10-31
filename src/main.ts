983e281e9071ede8afa1aeb371d1e480181fbb3d
Jang Rush <weakish@gmail.com>
2019-10-31 19:49:42 +0800

    feat: adapt for LeanEngine web hosting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
+  await app.listen(process.env.LEANCLOUD_APP_PORT, '0.0.0.0');
 }
 bootstrap();
